a0ac92fb2f28f305a48ca966bf98de1dc79e7ea0
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-11-12 17:58:27 -0500

    javascript example from pixabay

--- Front-end/colorlib-search-8/js/main.js
+++ Front-end/colorlib-search-8/js/main.js
@@ -1,5 +1,4 @@
-const app = document.getElementById('root')
-
+/*
 const logo = document.createElement('img')
 logo.src = 'logo.png'
 
@@ -8,35 +7,26 @@ container.setAttribute('class', 'container')
 
 app.appendChild(logo)
 app.appendChild(container)
-
-var request = new XMLHttpRequest()
+*/
+var API_KEY = '14260700-2be5fa852c1142d527bf48083';
+var URL = "https://pixabay.com/api/https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+/*var request = new XMLHttpRequest()
 request.open('GET', 'https://pixabay.com/api/https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
 request.onload = function() {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-
-      const h1 = document.createElement('h1')
-      h1.textContent = movie.title
-
-      const p = document.createElement('p')
-      movie.description = movie.description.substring(0, 300)
-      p.textContent = `${movie.description}...`
-
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
-  }
+  data.forEach(id => {
+    console.log(id)
+  })
 
 }
 
 
 request.send()
+*/
+$.getJSON(URL, function(data){
+if (parseInt(data.totalHits) > 0)
+    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+else
+    console.log('No hits');
+});
