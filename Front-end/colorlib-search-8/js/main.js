0163786bc0b3a8a5eac09b57568d637dedf6be2c
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-11-12 18:25:19 -0500

    Update main.js

--- Front-end/colorlib-search-8/js/main.js
+++ Front-end/colorlib-search-8/js/main.js
@@ -9,9 +9,9 @@ app.appendChild(logo)
 app.appendChild(container)
 */
 var API_KEY = '14260700-2be5fa852c1142d527bf48083';
-var URL = "https://pixabay.com/api/https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+var URL = "https://pixabay.com/api/https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent();
 var request = new XMLHttpRequest()
-request.open('GET', 'https://pixabay.com/api/https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
+request.open('GET', 'https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
 request.send()
 $.getJSON(URL, function(data){
 if (parseInt(data.totalHits) > 0)
