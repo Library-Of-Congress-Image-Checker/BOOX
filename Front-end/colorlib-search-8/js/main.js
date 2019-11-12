98a94c5a75c8a989b270b5048bb43e5c76c87a95
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-11-12 18:04:10 -0500

    took out comment thing

--- Front-end/colorlib-search-8/js/main.js
+++ Front-end/colorlib-search-8/js/main.js
@@ -10,7 +10,7 @@ app.appendChild(container)
 */
 var API_KEY = '14260700-2be5fa852c1142d527bf48083';
 var URL = "https://pixabay.com/api/https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-/*var request = new XMLHttpRequest()
+var request = new XMLHttpRequest()
 request.open('GET', 'https://pixabay.com/api/https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
 request.send()
 $.getJSON(URL, function(data){
