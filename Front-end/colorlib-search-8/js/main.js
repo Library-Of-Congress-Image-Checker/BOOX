42b92c0c1b00753ec787e1ce7ae62aace60e2506
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-11-12 19:58:16 -0500

    test2

--- Front-end/colorlib-search-8/js/main.js
+++ Front-end/colorlib-search-8/js/main.js
@@ -12,8 +12,8 @@ app.appendChild(container)
 var API_KEY = '14260700-2be5fa852c1142d527bf48083';
 var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('horse');
 var request = new XMLHttpRequest()
-request.open('GET', 'https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
-request.send()
+//request.open('GET', 'https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
+//request.send()
 $.getJSON(URL, function(data){
 if (parseInt(data.totalHits) > 0)
     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
