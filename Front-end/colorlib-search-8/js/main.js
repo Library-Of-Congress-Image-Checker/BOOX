0f542678dfbf2ae52c5f9d46c7410cdeab157ebb
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-11-12 20:27:58 -0500

    onclick

--- Front-end/colorlib-search-8/js/main.js
+++ Front-end/colorlib-search-8/js/main.js
@@ -8,12 +8,13 @@ container.setAttribute('class', 'container')
 app.appendChild(logo)
 app.appendChild(container)
 */
-//var input = document.getElementById("search");
+
+function searchBar() {
+  var input = document.getElementById("search").value;
+}
 var API_KEY = '14260700-2be5fa852c1142d527bf48083';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('horse');
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(input);
 var request = new XMLHttpRequest()
-//request.open('GET', 'https://pixabay.com/api/?key=14260700-2be5fa852c1142d527bf48083', true)
-//request.send()
 $.getJSON(URL, function(data){
 if (parseInt(data.totalHits) > 0)
     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
