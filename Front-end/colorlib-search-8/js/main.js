08b2122ea5f9f6d6cca165cef992a5ae4722fd51
ggellimac <32497210+ggellimac@users.noreply.github.com>
2019-11-12 20:32:12 -0500

    put everything in the searchbar function

--- Front-end/colorlib-search-8/js/main.js
+++ Front-end/colorlib-search-8/js/main.js
@@ -11,13 +11,14 @@ app.appendChild(container)
 var input = '';
 function searchBar() {
   input = document.getElementById("search").value;
+  var API_KEY = '14260700-2be5fa852c1142d527bf48083';
+  var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(input);
+  var request = new XMLHttpRequest()
+  $.getJSON(URL, function(data){
+  if (parseInt(data.totalHits) > 0)
+      $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+  else
+      console.log('No hits');
+  });
+
 }
-var API_KEY = '14260700-2be5fa852c1142d527bf48083';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(input);
-var request = new XMLHttpRequest()
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-    console.log('No hits');
-});
